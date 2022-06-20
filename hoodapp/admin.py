2472d26fc586a3c47fa492816bfcc51480ded8d4
muthonimuriithi <loiss.muthoni@student.moringaschool.com>
2022-06-20 07:04:01 +0300

    ft: registered my models

--- hoodapp/admin.py
+++ hoodapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile,Neighbourhood,Business,Post
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Neighbourhood)
+admin.site.register(Business)
+admin.site.register(Post)
